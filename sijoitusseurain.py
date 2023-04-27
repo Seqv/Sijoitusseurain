374ed6d8b4391007aa6d1337ac7ddd95f4760af6
Seqv <36482151+Seqv@users.noreply.github.com>
2023-04-27 16:09:38 +0300

    Update sijoitusseurain.py

--- sijoitusseurain.py
+++ sijoitusseurain.py
@@ -1,10 +1,36 @@
 import json
 import subprocess
 import sys
-import boto3
 import os
 
-# Asentaa yahoo financen
+import json
+
+with open('config.json','r') as config_file:
+    data = json.load(config_file)
+
+
+discordwebhook = data['webhook']
+
+sijoitus1ticker = data['sijoitus1ticker']
+sijoitus2ticker = data['sijoitus2ticker']
+sijoitus3ticker = data['sijoitus3ticker']
+sijoitus4ticker = data['sijoitus4ticker']
+sijoitus5ticker = data['sijoitus5ticker']
+sijoitus6ticker = data['sijoitus6ticker']
+sijoitus7ticker = data['sijoitus7ticker']
+sijoitus8ticker = data['sijoitus8ticker']
+
+sijoitus1nimi = data['sijoitus1nimi']
+sijoitus2nimi = data['sijoitus2nimi']
+sijoitus3nimi = data['sijoitus3nimi']
+sijoitus4nimi = data['sijoitus4nimi']
+sijoitus5nimi = data['sijoitus5nimi']
+sijoitus6nimi = data['sijoitus6nimi']
+sijoitus7nimi = data['sijoitus7nimi']
+sijoitus8nimi = data['sijoitus8nimi']
+
+
+# Asentaa yahoo finance- discord.py- ja requests-moduulit.
 subprocess.check_call([sys.executable, "-m", "pip", "install", "--target", "/tmp", 'yfinance'])
 sys.path.append('/tmp')
 subprocess.check_call([sys.executable, "-m", "pip", "install", "--target", "/tmp", 'requests'])
@@ -16,16 +42,15 @@ from discord import SyncWebhook
 
 import yfinance
 
-# Asettaa pörssiosakkeen YAHOO FINANCE-tickerin (jos haluat lisätä omia osakkeita, hae kyseistä osaketta sivulta finance.yahoo.com ja kopioi koodi.)
-sijoitus1=yfinance.Ticker('KEMPOWR.HE')
-sijoitus2=yfinance.Ticker('0P000134K9.F')
-sijoitus3=yfinance.Ticker('VIK1V.HE')
-sijoitus4=yfinance.Ticker('NDA-FI.HE')
-sijoitus5=yfinance.Ticker('FIA1S.HE')
-sijoitus6=yfinance.Ticker('VALMT.HE')
-sijoitus7=yfinance.Ticker('ELISA.HE')
-sijoitus8=yfinance.Ticker('XDW0.DE')
-#sijoitus9nyt = hintanyt('<YAHOOFINANCETICKERTÄHÄN>') # Sijoitus9 on esimerkkipohja, joka ohjeistaa, miten lisätä omia osakkeita. Sen jälkeen kun olet määrittänyt kaikki vaaditut arvot (löytyy README-tiedoston käyttö-osioista), poista #-merkki kaikkien esimerkkipohjarivien aluista, jotta rivit eivät ole enää kommentteina, vaan python suorittaa ne.
+# Asettaa pörssiosakkeen YAHOO FINANCE-tickerin (löytääksesi omien sijoitusten tickerin, hae kyseistä osaketta sivulta finance.yahoo.com ja kopioi koodi.)
+sijoitus1=yfinance.Ticker(sijoitus1ticker)
+sijoitus2=yfinance.Ticker(sijoitus2ticker)
+sijoitus3=yfinance.Ticker(sijoitus3ticker)
+sijoitus4=yfinance.Ticker(sijoitus4ticker)
+sijoitus5=yfinance.Ticker(sijoitus5ticker)
+sijoitus6=yfinance.Ticker(sijoitus6ticker)
+sijoitus7=yfinance.Ticker(sijoitus7ticker)
+sijoitus8=yfinance.Ticker(sijoitus8ticker)
 
 
 sijoitus1nyt = 0
@@ -36,28 +61,26 @@ sijoitus5nyt = 0
 sijoitus6nyt = 0
 sijoitus7nyt = 0
 sijoitus8nyt = 0
-#sijoitus9nyt = 0 # tätä ei kannata eikä tarvitse muuttaa. Tämän tarkoitus on vaan, että muuttujalle on suoritettu "declaration". Arvo voisi olla mikä tahansa tasaluku, mutta se ei muuttaisi koodin toimintaa mitenkään.
-
 
 def hintanyt(symbol): # EI TOIMI RAHASTOILLA KOSKA NIILLÄ EI OLE "ARVOA"
     ticker = yfinance.Ticker(symbol)
     todays_data = ticker.history(period='1d')
     return todays_data['Close'][0]
-sijoitus1nyt = hintanyt('KEMPOWR.HE')
-#sijoitus2nyt = hintanyt('0P000134K9.F') # ESIMERKKIOHJELMAN SIJOITUSKOHDE 2 ON RAHASTO - TÄMÄ TOIMINTO EI TOIMI RAHASTOILLA
-sijoitus3nyt = hintanyt('VIK1V.HE')
-sijoitus4nyt = hintanyt('NDA-FI.HE')
-sijoitus5nyt = hintanyt('FIA1S.HE')
-sijoitus6nyt = hintanyt('VALMT.HE')
-sijoitus7nyt = hintanyt('ELISA.HE')
-sijoitus8nyt = hintanyt('XDW0.DE')
-#sijoitus9nyt = hintanyt('<YAHOOFINANCETICKERTÄHÄN>') # Laita tähänkin osakkeesi yahoo finance-ticker.
+
+sijoitus1nyt = hintanyt(sijoitus1ticker)
+#sijoitus2nyt = hintanyt(sijoitus2ticker) # ESIMERKKIOHJELMAN SIJOITUSKOHDE 2 ON RAHASTO - TÄMÄ TOIMINTO EI TOIMI RAHASTOILLA
+sijoitus3nyt = hintanyt(sijoitus3ticker)
+sijoitus4nyt = hintanyt(sijoitus4ticker)
+sijoitus5nyt = hintanyt(sijoitus5ticker)
+sijoitus6nyt = hintanyt(sijoitus6ticker)
+sijoitus7nyt = hintanyt(sijoitus7ticker)
+sijoitus8nyt = hintanyt(sijoitus8ticker)
 
 
+#import boto3   ### TÄMÄ RIVI KOODIA MAHDOLLISTAA OHJELMAN TOIMIMISEN AWS LAMBDASSA, MUTTA TUO LAMBDAN ULKOPUOLELLA TURHAN RIIPPUVUUDEN. AWS LAMBDASSA POISTA TÄMÄN RIVIN KOMMENTOINTI POISTAMALLA # RIVIN ALUSTA
+#def lambda_handler(event, context):  ### TÄMÄ RIVI KOODIA MAHDOLLISTAA OHJELMAN TOIMIMISEN AWS LAMBDASSA MUTTA RIKKOO OHJELMAN LAMBDAN ULKOPUOLELLA - AWS LAMBDASSA POISTA TÄMÄN RIVIN KOMMENTOINTI POISTAMALLA # RIVIN ALUSTA.
 
-#def lambda_handler(event, context):  ### TÄMÄ RIVI KOODIA MAHDOLLISTAA OHJELMAN TOIMIMISEN AWS LAMBDASSA - LAMBDAN ULKOPUOLELLA KÄYTTÄESSÄ MUUTA AINA TÄMÄ RIVI KOMMENTIKSI LISÄÄMÄLLÄ # RIVIN ALKUUN
-
-if 1 == 1:   ### JOS KÄYTÄT TÄTÄ LAMBDAN ULKOPUOLELLA, POISTA TÄMÄN RIVIN ENSIMMÄINEN #. NIMITTÄIN PYTHONISSA SISENNYKSILLÄ ON VÄLIÄ!
+if 1 == 1:   ### JOS KÄYTÄT TÄTÄ OHJELMAA AWS LAMBDASSA, LISÄÄ TÄMÄN RIVIN ALKUUN #. NIMITTÄIN PYTHONISSA SISENNYKSILLÄ ON VÄLIÄ!
 
     alkuviesti = "--- Sijoitusten arvon muutos viime päivän ajalta: ---\n"
 
@@ -110,29 +133,21 @@ if 1 == 1:   ### JOS KÄYTÄT TÄTÄ LAMBDAN ULKOPUOLELLA, POISTA TÄMÄN RIVIN 
     if sijoitus8_percent < 0:
         sijoitus8_arrow = ":arrow_down:"
 
-    # --- SIJOITUSKOHDE 9 - ESIMERKKIPOHJA, Tätä ei tarvitse muuttaa ollenkaan. ---
-    #sijoitus9_percent = 1 - (sijoitus9.fast_info['previousClose'] / sijoitus9.fast_info['lastPrice'])
-    #sijoitus9_arrow = ":arrow_up:"
-    #if sijoitus9_percent < 0:
-    #    sijoitus9_arrow = ":arrow_down:"
-
     # "kasaa" webhook-viestit osakkeiden tiedoista.
-    sijoitus1discord = "Kempower:    "+"Hinta:"+str(sijoitus1nyt)[:6]+"€ "+sijoitus1_arrow+"   {:.2%}\n".format(sijoitus1_percent)
-    sijoitus2discord = "Nordnet Indeksi Helsinki:   "+sijoitus2_arrow+"   {:.2%}\n".format(sijoitus2_percent)           #tämä rivi on lyhyempi, koska tämä esimerkkisijoituskohde on rahasto, jolla ei ole osakkeiden kaltaista "hintaa"
-    sijoitus3discord = "Viking Line:    "+"Hinta:"+str(sijoitus3nyt)[:6]+"€ "+sijoitus3_arrow+"   {:.2%}\n".format(sijoitus3_percent)
-    sijoitus4discord = "Nordea:    "+"Hinta:"+str(sijoitus4nyt)[:6]+"€ "+sijoitus4_arrow+"   {:.2%}\n".format(sijoitus4_percent)
-    sijoitus5discord = "Finnair:    "+"Hinta:"+str(sijoitus5nyt)[:6]+"€ "+sijoitus5_arrow+"   {:.2%}\n".format(sijoitus5_percent)
-    sijoitus6discord = "Valmet:    "+"Hinta:"+str(sijoitus6nyt)[:6]+"€ "+sijoitus6_arrow+"   {:.2%}\n".format(sijoitus6_percent)
-    sijoitus7discord = "Elisa:    "+"Hinta:"+str(sijoitus7nyt)[:6]+"€ "+sijoitus7_arrow+"   {:.2%}\n".format(sijoitus7_percent)
-    sijoitus8discord = "Energia-ETF:    "+"Hinta:"+str(sijoitus8nyt)[:6]+"€"+sijoitus8_arrow+"   {:.2%}\n".format(sijoitus8_percent)
-    #sijoitus9discord = "<SIJOITUSKOHTEEN NIMI>:    "+"Hinta:"+str(sijoitus9nyt)[:6]+"<VALUUTTAMERKKI>"+sijoitus9_arrow+"   {:.2%}\n".format(sijoitus8_percent)
+    sijoitus1discord = sijoitus1nimi+"Hinta:"+str(sijoitus1nyt)[:6]+"€ "+sijoitus1_arrow+"   {:.2%}\n".format(sijoitus1_percent)
+    sijoitus2discord = sijoitus2nimi+sijoitus2_arrow+"   {:.2%}\n".format(sijoitus2_percent)           #tämä rivi on lyhyempi, koska tämä esimerkkisijoituskohde on rahasto, jolla ei ole osakkeiden kaltaista "hintaa"
+    sijoitus3discord = sijoitus3nimi+"Hinta:"+str(sijoitus3nyt)[:6]+"€ "+sijoitus3_arrow+"   {:.2%}\n".format(sijoitus3_percent)
+    sijoitus4discord = sijoitus4nimi+"Hinta:"+str(sijoitus4nyt)[:6]+"€ "+sijoitus4_arrow+"   {:.2%}\n".format(sijoitus4_percent)
+    sijoitus5discord = sijoitus5nimi+"Hinta:"+str(sijoitus5nyt)[:6]+"€ "+sijoitus5_arrow+"   {:.2%}\n".format(sijoitus5_percent)
+    sijoitus6discord = sijoitus6nimi+"Hinta:"+str(sijoitus6nyt)[:6]+"€ "+sijoitus6_arrow+"   {:.2%}\n".format(sijoitus6_percent)
+    sijoitus7discord = sijoitus7nimi+"Hinta:"+str(sijoitus7nyt)[:6]+"€ "+sijoitus7_arrow+"   {:.2%}\n".format(sijoitus7_percent)
+    sijoitus8discord = sijoitus8nimi+"Hinta:"+str(sijoitus8nyt)[:6]+"€"+sijoitus8_arrow+"   {:.2%}\n".format(sijoitus8_percent)
 
-    yksiviesti = alkuviesti+sijoitus1discord+sijoitus2discord+sijoitus3discord+sijoitus4discord+sijoitus5discord+sijoitus6discord+sijoitus7discord+sijoitus8discord#+sijoitus9discord
-
-    ### HUOMIO! ### Jos aiot käyttää esimerkkipohjaa, muista poistaa # sijoitus8discordin ja sijoitus9discordin välistä ylempänä.
+    #"kasaa" yksittäisten osakkeiden viestit yhdeksi kokonaiseksi viestiksi.
+    yksiviesti = alkuviesti+sijoitus1discord+sijoitus2discord+sijoitus3discord+sijoitus4discord+sijoitus5discord+sijoitus6discord+sijoitus7discord+sijoitus8discord
 
 
     #lähettää webhookit discord-palvelimelle
 
-    webhook = SyncWebhook.from_url('WEBHOOK TÄHÄN')
+    webhook = SyncWebhook.from_url(discordwebhook)
     webhook.send(content=yksiviesti)
